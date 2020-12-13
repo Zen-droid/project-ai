6563035f4869c1b7d9a4e3ac1f681d6d985260a3
Skynet <vincent.wong001@binus.ac.id>
2020-12-13 18:22:19 +0700

    code cleanup

--- db_handler.py
+++ db_handler.py
@@ -1,56 +1,4 @@
 import sqlite3
-import json
-
-
-# conn = sqlite3.connect("employee_data.db")
-# c = conn.cursor()
-
-# c.execute("""
-#     CREATE TABLE MsEmployee(
-#         EmployeeID INTEGER PRIMARY KEY,
-#         Name text NOT NULL,
-#         Position text NOT NULL,
-#         Division text
-#     )
-# """)
-
-# c.execute("""
-#     CREATE TABLE Attendance(
-#         EmployeeID INTEGER,
-#         Time text,
-#         Status text,
-#         Date text,
-
-#         PRIMARY KEY(EmployeeID, Date)
-#     )
-# """)
-
-# c.execute("DROP TABLE Attendance")
-
-# data_list = []
-
-# with open("employee_data.json") as filereader:
-#     data_list = json.load(filereader)
-
-# emp_list = []
-
-# for data in data_list:
-#     temp = data["id"], data["name"], data["position"], data["division"]
-#     emp_list.append(temp)
-
-
-# c.executemany("INSERT INTO MsEmployee VALUES(?, ?, ?, ?)", emp_list)
-
-
-# c.execute("SELECT * FROM MsEmployee")
-# data = c.fetchall()
-
-# for n in data:
-#     print(n)
-
-# print("Success")
-# conn.commit()
-# conn.close()
 
 def get_employee_data(id=0):
     conn = sqlite3.connect("employee_data.db")
@@ -78,37 +26,16 @@ def insert_attendance_data(data):
     conn.close()
     return success
 
-def get_attendance_data(id=0):
+def get_attendance_data(id):
     conn = sqlite3.connect("employee_data.db")
     c = conn.cursor()
 
-    if id != 0:
-        c.execute("SELECT * FROM Attendance WHERE id=?", str(id))
+    c.execute("SELECT * FROM Attendance WHERE EmployeeID=?", str(id))
 
-    c.execute("SELECT * FROM Attendance")
+    # c.execute("SELECT * FROM Attendance")
     return c.fetchall()
 
 if __name__ == "__main__":
     data = get_employee_data(2)
-    print(data)
-    print(type(data))
-    # data = get_attendance_data(2)
-    # if not data:
-    #     print("Empty")
-    # else:
-    #     for x in data:
-    #         print(x)
-    
-    # data = (5, '13:14:48', 'Attended', '2020-12-14')
-
-    # status = insert_attendance_data(data)
-
-    # if status:
-    #     attendance_data = get_attendance_data()
-    #     for x in attendance_data:
-    #         print(x)
-    # else:
-    #     print("Duplicate Data")
-
-    print("eof")
+    pass
 
