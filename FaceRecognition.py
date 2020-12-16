6fad3a3eff22c8ea9dea820c136e313a04f8f5a7
Skynet <vincent.wong001@binus.ac.id>
2020-12-17 00:51:55 +0700

    added temperature

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -4,7 +4,7 @@ import face_recognition_knn
 import cv2
 import os
 import attendance_window
-import bounding_boxes
+import overlay
 
 
 def recognize():
@@ -12,10 +12,12 @@ def recognize():
     cap.set(3, 640)
     cap.set(4, 480)
 
+    temperature = 37.9
+
     while True:
         _, img = cap.read()
         
-        img = bounding_boxes.draw_overlay(img)
+        img = overlay.draw_overlay(img, temperature)
         cv2.imshow("Face Detection", img)
 
         # Scaling image down by 1/4 resolution for faster face recognition
@@ -26,8 +28,7 @@ def recognize():
 
         if len(face_bounding_boxes) == 1:
             print("Face Detected")
-            # original_rgb_img = cv2.resize(rgb_small_img, (0,0), fx=4, fy=4)
-            
+
             predictions = face_recognition_knn.predict(rgb_small_img, model_path="knn_model.clf")
             for name, (top, right, bottom, left) in predictions:
                 print("- Found {} at ({}, {})".format(name, left, top))
@@ -37,38 +38,22 @@ def recognize():
             present = attendance_window.check_attendance(name)
 
             if name != "unknown" and not present:
+                status = True if temperature < 37.5 else False
+                overlay.draw_attendance_status(img, predictions[0][1], status)
+                cv2.imshow("Face Detection", img)
+
                 name_path = os.path.join("data/train", name)
                 img_path = image_files_in_folder(name_path)[0]
-                attendance_window.show_attendance_window(img_path, int(name))
+                attendance_window.show_attendance_window(img_path, name)
 
-            # cap.release()
-            # cv2.destroyAllWindows()
             # return predictions[0][0]
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
+            cap.release()
+            cv2.destroyAllWindows()
             break
 
 if __name__ == "__main__":
-
-    print("(1) Train Model")
-    print("(2) Face Recognition")
-    user_input = int(input("Choose >> "))
-    # user_input = 2
-
-    if user_input == 1:
-        train_path = "data/train"
-        print(" -- (!) Training..")
-        classifier = face_recognition_knn.train(train_path, model_save_path="knn_model.clf", n_neighbors=1, verbose=True)
-        print(" -- (!) Training completed..")
-    else:
-        name = recognize()
-        # print(name)
-        # name_path = os.path.join("data/train", name)
-        # img_path = image_files_in_folder(name_path)[0]
-
-        # attendance_window.show_attendance_window(img_path)
-        # img = cv2.imread(img_path)
-        # rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # cv2.imshow("Result", img)
-        # cv2.waitKey(0)
+    print("Starting Face Recognition..")
+    name = recognize()
 
